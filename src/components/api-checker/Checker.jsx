d1aef9ba116059d026be0214016ecacf27995dd4
Mehrab <mmehrab.pk@gmail.com>
2026-02-17 22:41:29 +0330

    update

--- src/components/api-checker/Checker.jsx
+++ src/components/api-checker/Checker.jsx
@@ -3,7 +3,7 @@ import Header from './header/Header'
 
 export default function Checker() {
   return (
-    <div className={'h-175 w-[70%] rounded-[14px] border border-border'}>
+    <div className={'h-175 w-[55%] rounded-[14px] border border-border'}>
       <Header />
     </div>
   )
